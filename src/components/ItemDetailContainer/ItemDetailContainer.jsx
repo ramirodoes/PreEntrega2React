5c0591505564d28e58b9f5a6c4795a25d6b02557
ramirodoes <ramirodoes@gmail.com>
2024-03-28 17:47:56 -0300

    iniciando PF

--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -5,16 +5,31 @@ import './ItemDetailContainer.css';
 
 const ItemDetailContainer = ({ itemId }) => {
   const [product, setProduct] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    getProductById(itemId)
-      .then(response => {
-        setProduct(response);
-      })
-      .catch(error => {
+    const fetchProduct = async () => {
+      try {
+        setLoading(true);
+        const productData = await getProductById(parseInt(itemId)); // Convertir el ID a número
+        setProduct(productData);
+        setLoading(false);
+      } catch (error) {
         console.error(error);
-      });
+        setLoading(false);
+      }
+    };
+
+    fetchProduct();
   }, [itemId]);
+
+  if (loading) {
+    return <p>Cargando...</p>;
+  }
+
+  if (!product) {
+    return <p>No se encontró el producto.</p>;
+  }
 
   return (
     <div className='ItemDetailContainer'>
